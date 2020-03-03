b46afa76258f99ec5572bc893e873fe94434e262
@pearces17 <realguntur@student.undip.ac.id>
2020-03-03 15:51:55 +0700

    Ahay

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tugas Akhir versi 1
+
+Menggunakan Cognitive Service Azure untuk identifikasi dan dlib untuk deteksi wajah
+Program Cadangan karena bukan sesuai paper yang diharapkan
---
+++ train.py
@@ -0,0 +1,8 @@
+import cognitive_face as CF
+from global_variables import personGroupId
+
+Key = 'fe11152b086c457c92eb23fc4dabcc48'
+CF.Key.set(Key)
+
+res = CF.person_group.train(personGroupId)
+print res
